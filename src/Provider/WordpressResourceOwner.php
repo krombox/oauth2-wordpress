237562bcb908a7301bcc9734fe02737bc2f92760
Roman Kapustian <ikrombox@gmail.com>
2017-08-29 17:54:21 +0300

    initial commit

---
+++ src/Provider/WordpressResourceOwner.php
@@ -0,0 +1,90 @@
+<?php 
+
+namespace Krombox\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use League\OAuth2\Client\Tool\ArrayAccessorTrait;
+
+class WordpressResourceOwner implements ResourceOwnerInterface
+{
+    use ArrayAccessorTrait;
+
+    /**
+     * Raw response
+     *
+     * @var array
+     */
+    protected $response;
+
+    /**
+     * Creates new resource owner.
+     *
+     * @param array  $response
+     */
+    public function __construct(array $response = array())
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Get resource owner id
+     *
+     * @return string|null
+     */
+    public function getId()
+    {
+        //vardump($this->response);die();
+        return $this->getValueByKey($this->response, 'id');
+    }
+
+    /**
+     * Get resource owner email
+     *
+     * @return string|null
+     */
+    public function getEmail()
+    {
+        return $this->getValueByKey($this->response, 'email');
+    }    
+
+    /**
+     * Get resource owner username
+     *
+     * @return string|null
+     */
+    public function getUsername()
+    {
+        return $this->getValueByKey($this->response, 'username');
+    }
+
+    /**
+     * Get resource owner link
+     *
+     * @return string|null
+     */
+    public function getLink()
+    {
+        return $this->getValueByKey($this->response, 'link');
+    }
+
+    /**
+     * Get resource owner avatar url
+     *
+     * @return string|null
+     */
+    public function getAvatarUrl($size = 96)
+    {
+        $avatarUrls = $this->getValueByKey($this->response, 'avatar_urls');
+        return in_array($size, array('24','48','96')) ? $avatarUrls[$size] : null;        
+    }    
+
+    /**
+     * Return all of the owner details available as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->response;
+    }
+}
