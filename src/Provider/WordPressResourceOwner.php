fbfeae28f8374299fe11c2e416809f7efdb1fc0d
Roman Kapustian <ikrombox@gmail.com>
2024-02-12 11:02:24 +0200

    PSR2 coding standard

--- src/Provider/WordPressResourceOwner.php
+++ src/Provider/WordPressResourceOwner.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Krombox\OAuth2\Client\Provider;
 
@@ -7,84 +7,84 @@ use League\OAuth2\Client\Tool\ArrayAccessorTrait;
 
 class WordPressResourceOwner implements ResourceOwnerInterface
 {
-	use ArrayAccessorTrait;
+    use ArrayAccessorTrait;
 
-	/**
-	 * Raw response
-	 *
-	 * @var array
-	 */
-	protected $response;
+    /**
+     * Raw response
+     *
+     * @var array
+     */
+    protected $response;
 
-	/**
-	 * Creates new resource owner.
-	 *
-	 * @param array  $response
-	 */
-	public function __construct(array $response = array())
-	{
-		$this->response = $response;
-	}
+    /**
+     * Creates new resource owner.
+     *
+     * @param array  $response
+     */
+    public function __construct(array $response = array())
+    {
+        $this->response = $response;
+    }
 
-	/**
-	 * Get resource owner id
-	 *
-	 * @return string|null
-	 */
-	public function getId()
-	{
-		//vardump($this->response);die();
-		return $this->getValueByKey($this->response, 'id');
-	}
+    /**
+     * Get resource owner id
+     *
+     * @return string|null
+     */
+    public function getId()
+    {
+        //vardump($this->response);die();
+        return $this->getValueByKey($this->response, 'id');
+    }
 
-	/**
-	 * Get resource owner email
-	 *
-	 * @return string|null
-	 */
-	public function getEmail()
-	{
-		return $this->getValueByKey($this->response, 'email');
-	}    
+    /**
+     * Get resource owner email
+     *
+     * @return string|null
+     */
+    public function getEmail()
+    {
+        return $this->getValueByKey($this->response, 'email');
+    }
 
-	/**
-	 * Get resource owner username
-	 *
-	 * @return string|null
-	 */
-	public function getUsername()
-	{
-		return $this->getValueByKey($this->response, 'username');
-	}
+    /**
+     * Get resource owner username
+     *
+     * @return string|null
+     */
+    public function getUsername()
+    {
+        return $this->getValueByKey($this->response, 'username');
+    }
 
-	/**
-	 * Get resource owner link
-	 *
-	 * @return string|null
-	 */
-	public function getLink()
-	{
-		return $this->getValueByKey($this->response, 'link');
-	}
+    /**
+     * Get resource owner link
+     *
+     * @return string|null
+     */
+    public function getLink()
+    {
+        return $this->getValueByKey($this->response, 'link');
+    }
 
-	/**
-	 * Get resource owner avatar url
-	 *
-	 * @return string|null
-	 */
-	public function getAvatarUrl($size = 96)
-	{
-		$avatarUrls = $this->getValueByKey($this->response, 'avatar_urls');
-		return in_array($size, array('24','48','96')) ? $avatarUrls[$size] : null;        
-	}    
+    /**
+     * Get resource owner avatar url
+     *
+     * @return string|null
+     */
+    public function getAvatarUrl($size = 96)
+    {
+        $avatarUrls = $this->getValueByKey($this->response, 'avatar_urls');
+        return in_array($size, array('24','48','96')) ? $avatarUrls[$size] : null;
+    }
 
-	/**
-	 * Return all of the owner details available as an array.
-	 *
-	 * @return array
-	 */
-	public function toArray()
-	{
-		return $this->response;
-	}
+    /**
+     * Return all of the owner details available as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->response;
+    }
 }
